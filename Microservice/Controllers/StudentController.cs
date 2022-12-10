426a0153860056bae4ddc0a66c844a79aaa00261
Kirill <kirill.kazakov26@gmail.com>
2022-12-11 00:58:46 +0300

    added studentController

---
+++ Microservice/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microservice.Models;
+
+namespace Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : Controller
+    {
+        private ApplicationContext db = new ApplicationContext();
+        [HttpGet]
+        public IActionResult StudentView()
+        {
+            ViewBag.Students = db.Students.ToList();
+            return View();
+        }
+    }
+}
--- Microservice/Models/StudentContext.cs
+++ Microservice/Models/StudentContext.cs
@@ -1,6 +1,14 @@
-﻿namespace Microservice.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Microservice.Models
 {
-    public class StudentContext
+    public class StudentContext : DbContext
     {
+        public StudentContext(DbContextOptions<StudentContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
     }
+
 }
