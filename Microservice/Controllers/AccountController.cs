de070f950db2e956c37c285b73608b9c01af8760
Данила Ермолаев <ermolaew.danila2016@gmail.com>
2022-12-16 20:38:40 +0300

    Update authorization

--- Microservice/Controllers/AccountController.cs
+++ Microservice/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microservice.Models.Authorization;
 using Microservice.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Microservice.Controllers
 {
@@ -30,10 +31,27 @@ namespace Microservice.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginModel modelLogin)
         {
+            if (ModelState.IsValid)
+            {
+                var student = await _context.Students
+                    .FirstOrDefaultAsync(s => s.Email == modelLogin.Email && s.Password == modelLogin.Password);
+                if (student != null)
+                {
+                    await Authenticate(modelLogin.Email); // аутентификация
 
-            if (modelLogin.Email == "user@example.com" &&
+                    return RedirectToAction("Index", "Students");
+                }
+
+                ViewData["ValidateMessage"] = "user not found";
+                return View();
+
+            }
+            return View(modelLogin);
+
+            /*if (modelLogin.Email == "user@example.com" &&
                 modelLogin.Password == "123"
                 )
             {
@@ -62,7 +80,20 @@ namespace Microservice.Controllers
 
 
             ViewData["ValidateMessage"] = "user not found";
-            return View();
+            return View();*/
+        }
+
+        private async System.Threading.Tasks.Task Authenticate(string userName)
+        {
+            // создаем один claim
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+            // создаем объект ClaimsIdentity
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            // установка аутентификационных куки
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
         }
 
         [HttpGet]
